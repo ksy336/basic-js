2b681be23df94c265a8f5151fbbcbfbc867b6054
Kseniya Klimova <ksuoxy773@gmail.com>
2021-03-07 14:14:39 +0400

    feat: add solution to countCats

--- src/count-cats.js
+++ src/count-cats.js
@@ -5,19 +5,16 @@ module.exports = function countCats( matrix ) {
   if (arguments.length === 0 || matrix.length === 0) {
     return [];
   }
-  let cat = '^^';
-  let numberOfCats = 0;
-  for (let j = 0; j < matrix.length; j++) {
-    for (let n = 0; n < matrix[j].length; j++) {
-      if (matrix[j][n] === cat) {
-        return numberOfCats++;
-      }
 
-    }
+  return matrix.reduce(function (prev, curr) {
+   return prev.concat(curr)}, []).filter(function(item) {
+     return item === '^^';
+  }).length;
+
 
   }
 
-  return numberOfCats;
+
 };
 
 
