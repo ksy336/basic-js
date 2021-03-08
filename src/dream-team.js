d4ba95e4770099d879a1bc1605b100490c93197d
Kseniya Klimova <ksuoxy773@gmail.com>
2021-03-08 15:54:05 +0400

    fix: add solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,8 +1,8 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam( members ) {
+module.exports = function createDreamTeam(members) {
   if (!Array.isArray(members)){
     return false;
   }
-   return  members.filter((item) => typeof item === 'string').flatMap((item) => item.trim()[0].toUpperCase()).sort().join('');
+   return  members.filter(item => typeof item === 'string').flatMap(item => item.trim()[0].toUpperCase()).sort().join('');
 };
