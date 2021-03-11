297f22f596faad6df875377f7f870df9cf6f2b2e
Kseniya Klimova <ksuoxy773@gmail.com>
2021-03-11 14:04:13 +0400

    feat: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(string, options) {
+  let repeatTimes = options.repeatTimes === undefined ? '1' : options.repeatTimes;
+  let separator = options.separator === undefined ? '+' : options.separator;
+  let addition = options.addition === undefined ? '' : options.addition;
+  let additionRepeatTimes = options.additionRepeatTimes === undefined ? '1' : options.additionRepeatTimes;
+  let additionSeparator = options.additionSeparator === undefined ? '|' :  options.additionSeparator;
+
+  let newString = addition === '' ? '' : `${addition}${additionSeparator}`.repeat(additionRepeatTimes).slice(0, -additionSeparator.length);
+  return `${string}${newString}${separator}`.repeat(repeatTimes).slice(0, -separator.length);
+
 };
-  
